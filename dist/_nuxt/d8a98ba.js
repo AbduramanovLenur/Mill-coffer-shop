2a92503a988505ada87bee7844c51eff1c9004bc
Abduramanov Lenur <abdiramanovlenur@gmail.com>
2023-01-22 01:23:27 +0500

    update dist

---
+++ dist/_nuxt/d8a98ba.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{451:function(n,t,e){"use strict";e.r(t);var c={name:"Blog"},l=e(8),component=Object(l.a)(c,(function(){return(0,this._self._c)("main",{staticClass:"main"})}),[],!1,null,"5550eff8",null);t.default=component.exports}}]);
